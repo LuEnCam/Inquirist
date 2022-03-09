cdcb6ed31725ef21785344085733098211bcd59f
matthieu.barbot <matthieu.barbot@he-arc.ch>
2022-03-09 10:30:41 +0100

    init project

---
+++ demo/src/main/java/com/example/demo/InquiristApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquiristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquiristApplication.class, args);
+	}
+
+}
